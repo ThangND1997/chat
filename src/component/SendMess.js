7a8e19c38dc39b379fbed3ca02343754b982e18f
Thang Nguyen <thangnd@stageit.app>
2022-07-09 22:41:59 +0700

    push

--- src/component/SendMess.js
+++ src/component/SendMess.js
@@ -22,11 +22,13 @@ function SendMess ({ scroll }) {
             behavior: 'smooth',
             block: 'center'
         })
+        
     }
     return(
         <div style={{margin: 'auto'}}>
             <Input  placeholder="Nhập tin nhắn" value={message} onChange={e => setMessage(e.target.value)}/>
             <Button variant="contained" onClick={handleSendMess}>Gửi</Button>
+            <audio src="https://vnso-zn-15-tf-mp3-s1-zmp3.zmdcdn.me/f2418f925bd6b288ebc7/2993144109324927938?authen=exp=1657550185~acl=/f2418f925bd6b288ebc7/*~hmac=a31094e95f1e4f606ca2fcf1f4fa38c4&fs=MTY1NzM3NzM4NTgxNHx3ZWJWNnwwfDU0LjE3NS4xNzMdUngMzY" />
         </div>
     )
 }
